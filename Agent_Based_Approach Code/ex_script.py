bb87acbdbdf85dbd4ef102d9159d1c8b02bdff35
zstemple <37884716+zstemple@users.noreply.github.com>
2018-04-04 14:45:16 -0700

    Small updates

--- Agent_Based_Approach Code/ex_script.py
+++ Agent_Based_Approach Code/ex_script.py
@@ -10,16 +10,16 @@ from model import *
 import matplotlib.pyplot as plt
 
 
-model = ScientistModel(5, 2, 2)
-for i in range(2):
-    model.step()
+model = ScientistModel(2, 1, 5)
+for i in range(10):
+   model.step()
 
-#all_wealth = []
-#for j in range(100):
-#    model = ScientistModel(10, 2, 10)
-#    for i in range(10):
-#        model.step()
-#    for agent in model.schedule.agents:
-#        all_wealth.append(agent.wealth)
-#    
-#plt.hist(all_wealth, bins=range(max(all_wealth)+1))
+# all_wealth = []
+# for j in range(100):
+#     model = ScientistModel(10, 2, 10)
+#     for i in range(10):
+#         model.step()
+#     for agent in model.schedule.agents:
+#         all_wealth.append(agent.wealth)
+    
+# plt.hist(all_wealth, bins=range(max(all_wealth)+1))
--- Agent_Based_Approach Code/model.py
+++ Agent_Based_Approach Code/model.py
@@ -1,24 +1,26 @@
 from mesa import Agent, Model
 from mesa.time import BaseScheduler
+from mesa.datacollection import DataCollector
+import random
 import numpy as np
-from scipy.stats import logistic
-from numpy.random import poisson
+from numpy.random import poisson, logistic
 
 # Sigmoid function
 def logistic_cdf(x, loc, scale):
     return 1/(1+np.exp((loc-x)/scale))
 
-#Sigmoid function starting at x = 0, scale, shape > 0, x >= 0
+# Sigmoid function starting at x = 0, scale, shape > 0, x >= 0
 def gompertz_cdf(x, shape, scale):
     return 1-np.exp(-shape*(np.exp(scale*x)-1))
 
-def create_return_graph(num_ideas, max_of_max_inv, sds, means):
-    x = np.arange(max_of_max_inv+1)
+def create_return_matrix(num_ideas, max_of_max_inv, sds, means):
+    x = np.arange(max_of_max_inv + 1)
     returns_list = []
     for i in range(num_ideas):
-        returns = logistic.cdf(x,loc = means[i], scale = sds[i])
+        returns = logistic_cdf(x, loc = means[i], scale = sds[i])
+        # Stacks arrays horizontally
         to_subt_temp = np.hstack((0,returns[:-1]))
-        # calculates return per unit of effort
+        # Calculates return per unit of effort
         returns = returns - to_subt_temp
         returns_list.append(returns)
     return(np.array(returns_list))
@@ -34,7 +36,7 @@ class Scientist(Agent):
         self.start_effort_decay = 1
 
         # Scalar: amount of effort a scientist has left
-        self.avail_effort = self.start_effort #FIX
+        self.avail_effort = self.start_effort.copy() 
         
         # Investment cost for each idea for the scientist
         self.k = poisson(lam=2, size=model.total_ideas)
@@ -45,7 +47,7 @@ class Scientist(Agent):
         
         # Create the ideas/returns matrix
         self.returns_matrix = \
-            create_return_graph(model.total_ideas, max(model.max_investment), self.sds, self.means)
+            create_return_matrix(model.total_ideas, max(model.max_investment), sds, means)
         
         # Records when the scientist was born
         self.birth_time = model.schedule.time
@@ -54,16 +56,36 @@ class Scientist(Agent):
         self.effort_invested = np.zeros(model.total_ideas)
         
     def step(self):
-        print(self.unique_id)
-        if self.unique_id > 2:
-            self.model.schedule.remove(self)
+        print(self.returns_matrix)
 
+        # Check scientist's age in the current time period
+        self.current_age = model.schedule.time - self.birth_time
+
+        if self.current_age == 0 or self.current_age == 1:
+            # Determine how much effort a scientist has in the current time period
+            self.curr_period_effort = self.start_effort - self.start_effort_decay * self.current_age
+
+            # Insert code for optimizing effort allocation among ideas
+        else:
+            # Scientists are alive and able to do things for only two time periods
+            pass
+
+
+#        # Determine how much effort the scientist has in current time period
+#        self.curr_start_effort = self.start_effort - self.start_effort_decay*model.schedule.time
+#        print(self.unique_id)
+#        print(self.curr_start_effort)
+#        if self.wealth == 0:
+#            return
+#        other_agent = random.choice(self.model.schedule.agents)
+#        other_agent.wealth += 1
+#        self.wealth -= 1
 
 class ScientistModel(Model):
     def __init__(self, N, ideas_per_time, time_periods):
         self.num_scientists = N
         
-        self.total_ideas = ideas_per_time*time_periods
+        self.total_ideas = ideas_per_time * time_periods
         
         # Store the max investment allowed in any idea
         self.max_investment = poisson(lam=50, size=self.total_ideas)
