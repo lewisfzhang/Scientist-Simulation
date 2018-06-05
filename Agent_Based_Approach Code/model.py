f81e2737cc04ebb67706c8dacf66489aaffb58cd
mzhao94 <mzhao94@stanford.edu>
2018-06-04 21:59:09 -0700

    Commented the first half and cleaned up the print statements

--- Agent_Based_Approach Code/model.py
+++ Agent_Based_Approach Code/model.py
@@ -1,14 +1,3 @@
-# To Do List:
-# 1) Update old scientist code - DONE
-# 2) Figure out how to code base case (time period 0) - DONE
-# 3) Code edge case in which 80% of effort has been invested in only 1 idea - DONE
-# 4) Double check with Michelle that model needs to be passed to agent step - DONE
-# 5) Ask Michelle about referencing model class - DONE
-# 6) TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# 7) Need to update code to deal with invest cutoff issue
-#
-
-
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
@@ -17,18 +6,27 @@ import numpy as np
 from numpy.random import poisson, logistic
 import sys
 
-# Sigmoid function
+# Input: Parameters for the logistic cumulative distribution function
+# Output: Value at x of the logistic cdf defined by the location and scale parameter
 def logistic_cdf(x, loc, scale):
     return 1/(1+np.exp((loc-x)/scale))
 
-# Sigmoid function starting at x = 0, scale, shape > 0, x >= 0
-def gompertz_cdf(x, shape, scale):
-    return 1-np.exp(-shape*(np.exp(scale*x)-1))
-
+# Input: 
+# 1) num_ideas (scalar): number of ideas to create the return matrix for
+# 2) max_of_max_inv (scalar): the maximum of all the maximum investment limits over all ideas
+# 3) sds (array): the standard deviation parameters of the return curve of each idea
+# 4) means (array): the mean parameters of the return curve of each idea
+# Output:
+# A matrix that is has dimension n x m where n = num_ideas and m = max_of_max_inv
+# where each cell (i,j) contains the return based on the logistic cumulative 
+# distribution function of the i-th idea and the j-th extra unit of effort
 def create_return_matrix(num_ideas, max_of_max_inv, sds, means):
+
+    # Creates array of the effort units to calculate returns for
     x = np.arange(max_of_max_inv + 1)
     returns_list = []
     for i in range(num_ideas):
+        # Calculates the return for an idea for all amounts of effort units
         returns = logistic_cdf(x, loc = means[i], scale = sds[i])
         # Stacks arrays horizontally
         to_subt_temp = np.hstack((0,returns[:-1]))
@@ -37,8 +35,10 @@ def create_return_matrix(num_ideas, max_of_max_inv, sds, means):
         returns_list.append(returns)
     return(np.array(returns_list))
 
-# Program to return the second largest number in an array (will return the max
-# if there are more than one max numbers)
+# Input: 
+# 1) numbers (array): contains the numbers we are picking the second largest from
+# Output:
+# The second largest number out of the array 
 def second_largest(numbers):
     count = 0
     m1 = m2 = float('-inf')
@@ -52,40 +52,35 @@ def second_largest(numbers):
     return m2 if count >= 2 else None
 
 
-# Function to calculate "marginal" returns for available ideas, taking into account invest costs
+# Function to calculate "marginal" returns for available ideas, taking into account investment costs
 # Input:
-# 1) avail_ideas - array that indexes which ideas are available to a given scientist
-# 2) total_effort - array that contains cumulative effort already expended by all scientists
-# 3) max_investment - array that contains the max possible investment for each idea
-# 4) marginal_effort - array of "equivalent" marginal efforts, which equals
+# 1) avail_ideas (array): indexes which ideas are available to a given scientist
+# 2) total_effort (array):  contains cumulative effort already expended by all scientists
+# 3) max_investment (array): contains the max possible investment for each idea
+# 4) marginal_effort (array): "equivalent" marginal efforts, which equals
 #    the max, current investment cost plus one minus individual investment costs for available ideas
 #
 # Output:
-# 1) idx_max_return - scalar that is the index of the idea the scientist chose to invest in
-# 2) max_return - scalar that is the perceived return of the associated, chosen idea
+# 1) idx_max_return (scalar): the index of the idea the scientist chose to invest in
+# 2) max_return (scalar): the perceived return of the associated, chosen idea
     
 def calc_cum_returns(scientist, model):
-    print("Unique ID: ", scientist.unique_id)
-    print("Age of scientist: ", scientist.current_age)
-    print("Time period: ", model.schedule.time)
-    print("Current investment costs for all ideas: ", scientist.curr_k)
+
+    # Array: keeping track of all the returns of investing in each available ideas
     final_returns_avail_ideas = np.array([])
-    # Limit on the amount of effort that a scientist can invest in a single idea
+
+    # Scalar: limit on the amount of effort that a scientist can invest in a single idea
     # in one time period
     invest_cutoff = round(scientist.start_effort * 0.6)
 
-    
+    # Loops over all the ideas the scientist is allowed to invest in 
     for idea in np.where(scientist.avail_ideas == True)[0]:
+
         # OR Conditions
         # 1st) Edge case in which scientist doesn't have enough effort to invest in
         # an idea given the investment cost
-        # 2nd) Ensures that effort invested in ideas doesn't go over max invest
-        print("idea #: ", idea)
-        print("effort_left_in_idea:", scientist.effort_left_in_idea[idea])
+        # 2nd) Ensures that effort invested in ideas doesn't go over max investment limit
         if scientist.marginal_effort[idea] <= 0 or scientist.effort_left_in_idea[idea] == 0:
-            print("Amount invested in idea in period:", scientist.eff_inv_in_period[idea])
-            print("marginal effort:", scientist.marginal_effort[idea])
-#            print("invest cutoff:", invest_cutoff)
             final_returns_avail_ideas = np.append(final_returns_avail_ideas, 0)
 
         # For instances in which a scientist's marginal effort exceeds the 
@@ -97,53 +92,62 @@ def calc_cum_returns(scientist, model):
             returns = scientist.returns_matrix[idea, np.arange(start_index, stop_index)]
             total_return = sum(returns)
             final_returns_avail_ideas = np.append(final_returns_avail_ideas, total_return)
+
+        # The case in which there are no restrictions for investing in this idea
         else:
-            print("No restrictions on investing in this idea")
             start_index = int(model.total_effort[idea])
             stop_index = int(start_index + scientist.marginal_effort[idea])
             returns = scientist.returns_matrix[idea, np.arange(start_index, stop_index)]
             total_return = sum(returns)
             final_returns_avail_ideas = np.append(final_returns_avail_ideas, total_return)
 
-    
-    print("avail_effort: ", scientist.avail_effort)           
-    print("final_returns_avail_ideas: ", final_returns_avail_ideas)
-    print("Current investment costs: ", scientist.curr_k[np.where(scientist.avail_ideas == True)[0]])
-    print("Increment: ", scientist.increment)
+
+    # Finds the maximum return over all the available ideas
     max_return = max(final_returns_avail_ideas)
-    print("max_returns: ", max_return)
-    print(np.where(final_returns_avail_ideas == max_return))
+
+    # Finds the index of the maximum return over all the available ideas
     idx_max_return = np.where(final_returns_avail_ideas == max_return)[0]
-    print("idx_max_return: ", idx_max_return)
-    print("")
+
     # Resolves edge case in which there are multiple max returns. In this case,
     # check if each idea associated with the max return has reached the
     # investment cutoff. If it has reached the cutoff, remove it from the array
     # of ideas with max returns. Then randomly choose among the remaining ideas
+
+    # If there are ties for the maximum return
     if len(idx_max_return) > 1:
-#        print("Entered if statement. idx_max_return should be > 1")
-#        print("idx_max_return: ", idx_max_return)
-#        print("length of idx_max_return: ", len(idx_max_return))
+
+        # Array to keep track of updated index based on just the ideas that are tied for the max return
         upd_idx_max = np.array([])
+
+        # Iterate over all indices of the array containing the indices of the tied max returns
         for idx in np.nditer(idx_max_return):
+
+            # Deriving the correct index for the eff_inv_in_period array that contains all ideas that will
+            # ever be created instead of only this scientist's available ideas that idx_max_return is in 
+            # reference to
             idea_choice = idx + [(model.schedule.time + 1)*(model.ideas_per_time)] - len(final_returns_avail_ideas)
+
+            # If the scientist has exceeded her own limit on investing in an idea, skip to the next idea
             if scientist.eff_inv_in_period[idea_choice] >= invest_cutoff:
                 continue
+            # Otherwise append it to the updated-index-max array as an eligible idea
             else:
                 upd_idx_max = np.append(upd_idx_max, idea_choice)
-#        if upd_idx_max.size == 0:
-#            print("final_returns_avail_ideas: ", final_returns_avail_ideas)
-#            print("idx_max_return: ", idx_max_return)
-#            print("eff_inv_in_period:", scientist.eff_inv_in_period)
-#            sys.exit("upd_idx_max is empty")
+
+        # Randomly choose an idea over all tied, eligible ideas
         idea_choice = int(np.random.choice(upd_idx_max))
-        print("Hit investment cutoff!")
-        print("Effort invested in current period: ", scientist.eff_inv_in_period)
-        print("Next idea choice: ", idea_choice)
-        print("Second max return (equal to original max): ", max_return)
+
+        # Return both the idea_choice (this index is relative to the eff_inv_in_period array), and the max return
         return(idea_choice, max_return)
+
+    # Otherwise if there are no ties
     else:
+
+        # Deriving the correct index for the eff_inv_in_period array that contains all ideas that will
+        # ever be created instead of only this scientist's available ideas that idx_max_return is in 
+        # reference to
         idea_choice = idx_max_return[0] + [(model.schedule.time + 1)*(model.ideas_per_time)] - len(final_returns_avail_ideas)
+
         # Prevents scientists from investing further in a single idea if the
         # scientist has already invested at or above the investment cutoff.
         if scientist.eff_inv_in_period[idea_choice] >= invest_cutoff:
@@ -154,24 +158,27 @@ def calc_cum_returns(scientist, model):
             # scientist's current available effort. In this edge case, the
             # scientist is allowed to continue investing in an idea past
             # the investment cutoff
+
+            # The nested edge case where the scientist doesn't have any other ideas to invest in
+            # due to other restrictions
             if second_max == 0:
-                print("Just kidding. Can't invest in other ideas")
+                # Bypass the restriction on the investment cutoff in this case
                 return idea_choice, max_return
+            # If the scientist does have other ideas she can invest in 
             else:
+                # Find the idea with the second highest return
                 idx_second_max = np.where(final_returns_avail_ideas == second_max)[0]
+
+                # If there are ties for the second highest return
                 if len(idx_second_max) > 1:
+                    # Randomly choose between the tied ideas and derive the correct index in reference
+                    # to the eff_inv_in_period array that contains all ideas that will ever be created 
+                    # instead of only this scientist's available ideas that idx_max_return is in reference to
                     idea_choice = int(np.random.choice(idx_second_max)) + [(model.schedule.time + 1)*(model.ideas_per_time)] - len(final_returns_avail_ideas)
-                    print("Hit investment cutoff!")
-                    print("Effort invested in current period: ", scientist.eff_inv_in_period)
-                    print("Next idea choice: ", idea_choice)
-                    print("Second max return: ", second_max)
                     return(idea_choice, second_max)
+                # If there are no ties for the second highest return just return the only index
                 else:
                     idea_choice = idx_second_max[0] + [(model.schedule.time + 1)*(model.ideas_per_time)] - len(final_returns_avail_ideas)
-                    print("Hit investment cutoff!")
-                    print("Effort invested in current period: ", scientist.eff_inv_in_period)
-                    print("Next idea choice: ", idea_choice)
-                    print("Second max return: ", second_max)
                     return(idea_choice, second_max)
         return(idea_choice, max_return)
     
