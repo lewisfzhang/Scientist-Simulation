5375c5739afe46e1e8009d7c006785b5eb67e8d4
Lewis Zhang <Lewis.Zhang19@bcp.org>
2018-07-17 15:00:34 -0700

    updated init default configurations

--- src/init.py
+++ src/init.py
@@ -7,17 +7,17 @@ seed = np.random.randint(100000, 999999)
 
 # number of stable time_periods in the model
 # NOTE: total time periods is time_periods + 2 (first two are unstable)
-time_periods = 10
+time_periods = 50
 
 # Scalar: number of ideas unique to each time period
-ideas_per_time = 5
+ideas_per_time = 20
 
 # Scalar: number of scientists born per time period
 # NOTE: according to jay, N should be greater than ideas_per_time
-N = 10
+N = 40
 
 # the number of TP a scientist can actively invest in ideas
-time_periods_alive = 4
+time_periods_alive = 10
 
 # SCALAR: store means of the mean and sds for returns
 true_means_lam = 300
