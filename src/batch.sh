187bcbb41f997a67a6741a13a223898bd603a162
Lewis Zhang <Lewis.Zhang19@bcp.org>
2018-07-18 12:26:55 -0700

    small batch works!

--- src/batch.sh
+++ src/batch.sh
@@ -3,27 +3,38 @@
 #SBATCH --job-name=batch.sh                   # Job name
 #SBATCH --mail-type=END,FAIL                  # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=lewis.zhang19@bcp.org     # Where to send mail
-#SBATCH --ntasks=1                            # Run on a single CPU
-#SBATCH --mem=100mb                           # Job memory request
-#SBATCH --time=00:10:00                       # Time limit hrs:min:sec
-#SBATCH --output=debug/batch_%j.log     # Standard output and error log
+#SBATCH --ntasks=4                            # Run on a single CPU
+#SBATCH --mem=10gb                            # Job memory request
+#SBATCH --time=00:30:00                       # Time limit hrs:min:sec
+#SBATCH --output=debug/batch_%j.log           # Standard output and error log
 
-pwd; hostname; date
+# if we are running on supercomputer
+if [ $(cd ~ && pwd) == /home/users/lewisz ]; then
+    ml reset
+    ml load python/3.6.1
+fi
 
-ml reset
-ml load python/3.6.1
+echo "running batch.sh script"
 
-echo "Running batch.sh script"
+cd $(git rev-parse --show-toplevel)/src
 
-storage_dir=$PI_HOME/lewisz/batch_$(date +%d-%b-%H_%M)
-echo $storage_dir > $HOME/batch/storage_dir.txt
-storage_dir=$(cat $HOME/batch/storage_dir.txt)
-mkdir $storage_dir
+if [ $# -gt 0 -a $# -lt 4 ]; then
+    echo $0 need 4 parameter values
+    exit 1
+fi
 
-time_periods=50
-ideas_per_time=20
-N=40
-time_periods_alive=10
+if [ $# -eq 4 ]; then
+    time_periods=$1
+    ideas_per_time=$2
+    N=$3
+    time_periods_alive=$4
+elif [ $# -eq 0 ]; then
+    time_periods=50
+    ideas_per_time=20
+    N=40
+    time_periods_alive=10
+fi
+
 count=1
 
 for prop_sds in 0.2 0.4 0.6 0.8
@@ -32,27 +43,12 @@ do
     do
         for prop_start in 0.1 0.25 0.4 0.5 0.6 0.75
         do
-            echo $time_periods $ideas_per_time $N $time_periods_alive $prop_sds $prop_means $prop_start > $HOME/batch/init.txt
-            echo $count > $HOME/batch/job_count.txt
-            count=$((count + 1))
-            echo $d to $storage_dir/run_$(cat $HOME/batch/init.txt | tr ' ' '_')
-            for d in $PI_HOME/lewisz/storage_batch/*/; do mv $d $storage_dir/run_$(cat $HOME/batch/init.txt | tr ' ' '_'); break; done
-            # cp -r $HOME/Scientist-Simulation/ $storage_dir/run_$(cat $HOME/batch/init.txt | tr ' ' '_')
-            echo $(cat $HOME/batch/init.txt) > $storage_dir/run_$(cat $HOME/batch/init.txt | tr ' ' '_')/src/tmp/init.txt
-            sh $storage_dir/run_$(cat $HOME/batch/init.txt | tr ' ' '_')/src/run_batch.sh
+            echo $time_periods $ideas_per_time $N $time_periods_alive $prop_sds $prop_means $prop_start > tmp/init.txt
+            echo -e "\n\n\n"trial $count with configurations $(cat tmp/init.txt)"\n\n"
+            ./run.sh $(cat tmp/init.txt)
+            count=$((count+1))
         done
     done
 done
 
-
-# squeue -u lewisz > queue.txt
-# num=wc -l queue.txt
-# while [ $num -ne 1 ]; do
-#     sleep 5  # sleep 5 seconds
-#     squeue -u lewisz > queue.txt
-#     num=wc -l queue.txt
-# done
-#
-# rm queue.txt
-echo "all tasks completed"
-date
+echo "finished running batch.sh script"
